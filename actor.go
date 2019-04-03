c48c84ba1d8d1fcc14e3d7104134069754d68209
skyforce77 <usulguru@gmail.com>
2019-04-03 14:58:43 +0200

    Added reducer

--- actor.go
+++ actor.go
@@ -1,31 +1,65 @@
 package TinyActors
 
-type ActorType struct {
+type ActorModel struct {
 	mailbox chan *Message
-	action  func(*Message)
 	system  *System
+	run func(actor *Actor)
 }
 
-func (system *System) Declare(action func(*Message)) *ActorType {
-	typ := &ActorType{
+func (system *System) Declare(action func(*Message)) *ActorModel {
+	typ := &ActorModel{
 		make(chan *Message, 500),
-		action,
 		system,
+		func(actor *Actor) {
+			for {
+				v, ok := <-actor.mailbox
+				if ok {
+					action(v)
+				}
+				if !ok || actor.dropped {
+					break
+				}
+			}
+		},
 	}
 	system.declare(typ)
 	return typ
 }
 
-func (typ *ActorType) Forward(message *Message) {
+func (system *System) DeclareReducer(size int, action func([]*Message)) *ActorModel {
+	typ := &ActorModel{
+		make(chan *Message, 500),
+		system,
+		func(actor *Actor) {
+			for {
+				buff := make([]*Message, size)
+				for i:=0; i<size; i++ {
+					v, ok := <-actor.mailbox
+					if ok {
+						buff[i] = v
+					}
+					if !ok || actor.dropped {
+						return
+					}
+				}
+				action(buff)
+			}
+		},
+	}
+	system.declare(typ)
+	return typ
+}
+
+func (typ *ActorModel) Forward(message *Message) {
 	typ.mailbox <- message
 }
 
-func (typ *ActorType) Tell(value interface{}) {
+func (typ *ActorModel) Tell(value interface{}) {
 	message := newMessage(value)
 	typ.mailbox <- message
 }
 
-func (typ *ActorType) instanciate() *Actor {
+func (typ *ActorModel) instanciate() *Actor {
 	return &Actor{
 		typ,
 		false,
@@ -33,18 +67,6 @@ func (typ *ActorType) instanciate() *Actor {
 }
 
 type Actor struct {
-	*ActorType
+	*ActorModel
 	dropped bool
-}
-
-func (actor *Actor) run() {
-	for {
-		v, ok := <-actor.mailbox
-		if ok {
-			actor.action(v)
-		}
-		if !ok || actor.dropped {
-			break
-		}
-	}
 }
